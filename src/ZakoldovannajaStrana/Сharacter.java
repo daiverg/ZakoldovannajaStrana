e854bd3e2850472b503636fc72f1f8d66b68afe9
daiverg <43170010+daiverg@users.noreply.github.com>
2019-03-10 14:45:17 +0100

    add class Character

---
+++ src/ZakoldovannajaStrana/Сharacter.java
@@ -0,0 +1,21 @@
+package ZakoldovannajaStrana;
+
+public abstract class Сharacter {
+
+    private String characterClass;
+    private int strenght;
+    private int agility;
+    private int wisdom;
+    private int composition;
+    private int courage;
+//    greatsword ???
+    private int capacity;
+    private int riskPoint;
+    private int defenceClass;
+//    magic&stuff ???
+    private int level;
+//    animals ???
+    private int gamePoint;
+
+
+}
